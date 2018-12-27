52e01be95d14e4fcc88d9433398098f51fe75cd6
liudong <liudong.edward@gmail.com>
2018-12-27 16:08:39 +0800

    移除 JSON/XML 依赖

--- Sources/RequestAPI.swift
+++ Sources/RequestAPI.swift
@@ -1,31 +1,29 @@
 //
-//  XMLRequestAPI.swift
+//  RequestAPI.swift
 //  ANBaseNetwork
 //
-//  Created by 刘栋 on 2018/12/20.
+//  Created by 刘栋 on 2018/7/2.
 //  Copyright © 2018 anotheren.com. All rights reserved.
 //
 
 import Foundation
 import Alamofire
-import SWXMLHash
 
-public protocol XMLRequestAPI: BaseAPI {
+public protocol RequestAPI: BaseAPI {
     
     associatedtype ResultType
     
-    func handle(xml: XMLIndexer) -> Result<ResultType>
+    func handle(data: Data) -> Result<ResultType>
 }
 
 @discardableResult
-public func request<T: XMLRequestAPI>(api: T, completion: @escaping (Result<T.ResultType>) -> Void) -> Alamofire.DataRequest {
+public func request<T: RequestAPI>(api: T, completion: @escaping (Result<T.ResultType>) -> Void) -> Alamofire.DataRequest {
     return request(api.url, method: api.method, parameters: api.parameters, encoding: api.encoding, headers: api.headers).responseData { response in
         switch response.result {
         case let .failure(error):
             completion(.failure(error))
         case let .success(data):
-            let xml = SWXMLHash.parse(data)
-            completion(api.handle(xml: xml))
+            completion(api.handle(data: data))
         }
     }
 }
--- Sources/UploadAPI.swift
+++ Sources/UploadAPI.swift
@@ -1,5 +1,5 @@
 //
-//  JSONUploadAPI.swift
+//  UploadAPI.swift
 //  ANBaseNetwork
 //
 //  Created by 刘栋 on 2018/12/27.
@@ -8,30 +8,40 @@
 
 import Foundation
 import Alamofire
-import SwiftyJSON
 
-public protocol JSONUploadAPI: BaseAPI {
+public protocol UploadAPI: BaseAPI {
     
     associatedtype ResultType
     
     var multipartFormData: ((Alamofire.MultipartFormData) -> Void) { get }
     
-    func handle(json: JSON) -> Alamofire.Result<ResultType>
+    var uploadProcess: ((Progress) -> Void)? { get }
+    
+    func handle(data: Data) -> Alamofire.Result<ResultType>
 }
 
-public func upload<T: JSONUploadAPI>(api: T, completion: @escaping (Alamofire.Result<T.ResultType>) -> Void) {
+extension UploadAPI {
+    
+    public var uploadProcess: ((Progress) -> Void)? {
+        return nil
+    }
+}
+
+public func upload<T: UploadAPI>(api: T, completion: @escaping (Alamofire.Result<T.ResultType>) -> Void) {
     Alamofire.upload(multipartFormData: api.multipartFormData, to: api.url, method: api.method, headers: api.headers, encodingCompletion: { encodingResult in
         switch encodingResult {
         case .failure(let error):
             completion(.failure(error))
         case .success(let request, _, _):
-            request.responseJSON(completionHandler: { response in
+            request.uploadProgress(closure: { process in
+                api.uploadProcess?(process)
+            })
+            request.responseData(completionHandler: { response in
                 switch response.result {
-                case let .failure(error):
+                case .failure(let error):
                     completion(Result.failure(error))
-                case let .success(object):
-                    let json = JSON(object)
-                    completion(api.handle(json: json))
+                case .success(let data):
+                    completion(api.handle(data: data))
                 }
             })
         }
