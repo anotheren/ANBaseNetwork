d71ce4f86b0db223e3f3a2727edf6e17c001118d
liudong <liudong.edward@gmail.com>
2018-12-27 16:32:32 +0800

    Revert "移除 JSON/XML 依赖"
    This reverts commit 52e01be95d14e4fcc88d9433398098f51fe75cd6.

--- Sources/JSONUploadAPI.swift
+++ Sources/JSONUploadAPI.swift
@@ -1,5 +1,5 @@
 //
-//  UploadAPI.swift
+//  JSONUploadAPI.swift
 //  ANBaseNetwork
 //
 //  Created by 刘栋 on 2018/12/27.
@@ -8,40 +8,30 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
-public protocol UploadAPI: BaseAPI {
+public protocol JSONUploadAPI: BaseAPI {
     
     associatedtype ResultType
     
     var multipartFormData: ((Alamofire.MultipartFormData) -> Void) { get }
     
-    var uploadProcess: ((Progress) -> Void)? { get }
-    
-    func handle(data: Data) -> Alamofire.Result<ResultType>
+    func handle(json: JSON) -> Alamofire.Result<ResultType>
 }
 
-extension UploadAPI {
-    
-    public var uploadProcess: ((Progress) -> Void)? {
-        return nil
-    }
-}
-
-public func upload<T: UploadAPI>(api: T, completion: @escaping (Alamofire.Result<T.ResultType>) -> Void) {
+public func upload<T: JSONUploadAPI>(api: T, completion: @escaping (Alamofire.Result<T.ResultType>) -> Void) {
     Alamofire.upload(multipartFormData: api.multipartFormData, to: api.url, method: api.method, headers: api.headers, encodingCompletion: { encodingResult in
         switch encodingResult {
         case .failure(let error):
             completion(.failure(error))
         case .success(let request, _, _):
-            request.uploadProgress(closure: { process in
-                api.uploadProcess?(process)
-            })
-            request.responseData(completionHandler: { response in
+            request.responseJSON(completionHandler: { response in
                 switch response.result {
-                case .failure(let error):
+                case let .failure(error):
                     completion(Result.failure(error))
-                case .success(let data):
-                    completion(api.handle(data: data))
+                case let .success(object):
+                    let json = JSON(object)
+                    completion(api.handle(json: json))
                 }
             })
         }
--- Sources/XMLRequestAPI.swift
+++ Sources/XMLRequestAPI.swift
@@ -1,29 +1,31 @@
 //
-//  RequestAPI.swift
+//  XMLRequestAPI.swift
 //  ANBaseNetwork
 //
-//  Created by 刘栋 on 2018/7/2.
+//  Created by 刘栋 on 2018/12/20.
 //  Copyright © 2018 anotheren.com. All rights reserved.
 //
 
 import Foundation
 import Alamofire
+import SWXMLHash
 
-public protocol RequestAPI: BaseAPI {
+public protocol XMLRequestAPI: BaseAPI {
     
     associatedtype ResultType
     
-    func handle(data: Data) -> Result<ResultType>
+    func handle(xml: XMLIndexer) -> Result<ResultType>
 }
 
 @discardableResult
-public func request<T: RequestAPI>(api: T, completion: @escaping (Result<T.ResultType>) -> Void) -> Alamofire.DataRequest {
+public func request<T: XMLRequestAPI>(api: T, completion: @escaping (Result<T.ResultType>) -> Void) -> Alamofire.DataRequest {
     return request(api.url, method: api.method, parameters: api.parameters, encoding: api.encoding, headers: api.headers).responseData { response in
         switch response.result {
         case let .failure(error):
             completion(.failure(error))
         case let .success(data):
-            completion(api.handle(data: data))
+            let xml = SWXMLHash.parse(data)
+            completion(api.handle(xml: xml))
         }
     }
 }
